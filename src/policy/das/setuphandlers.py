8c385d01486e48ea68cb8981e6e68e507f840cec
Benoît Suttor <bsuttor@cirb.irisnet.be>
2013-03-07 11:45:24 +0100

    clean setuphandler

--- src/policy/das/setuphandlers.py
+++ src/policy/das/setuphandlers.py
@@ -36,17 +36,5 @@ def setupVarious(context):
 
     site = context.getSite()
 
-    news = site.news
-    news.setExcludeFromNav(True)
-    news.reindexObject()
-  
-    events = site.events
-    events.setExcludeFromNav(True)
-    events.reindexObject()
-
-    Members = site.Members
-    Members.setExcludeFromNav(True)
-    Members.reindexObject()
-
-    setupLinguaFolders(site, logger)
-    setup_tool = context.getSetupTool()
+    if not getattr(site, 'fr', None):
+        setupLinguaFolders(site, logger)
